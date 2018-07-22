1a6f1690fc78d9311fc8c69d5678c16452c55194
Nat Sharpe <sharpe1890@gmail.com>
2018-07-21 21:10:03 -0400

    solved long vowel problem for JS 102

--- scripts/102.js
+++ scripts/102.js
@@ -36,7 +36,7 @@
 
 // printNumbersWhile(1, 10);
 
-// Exercise 2:
+// // Exercise 2:
 // var square = function (size, char) {
 //     var count1 = 0;
 //     while (count1 < size) {
@@ -53,7 +53,7 @@
 
 // square(5, '*');
 
-// Exercise 3:
+// // Exercise 3:
 // var box = function (height, width, char1, char2) {
 //     var count1 = 0;
 //     while (count1 < height) {
@@ -61,17 +61,15 @@
 //         var count2 = 0;
 //         while (count2 < width) {
 //             if (count1 === 0 || (count1 + 1) === height) {
-//                 line = line + char1;
-//                 count2++;
+//                 line += char1;
 //             }
 //             else if (count2 > 0 && count2 < (width - 1)) {
-//                 line = line + char2;
-//                 count2++;
+//                 line += char2;
 //             }
 //             else {
-//                 line = line + char1;
-//                 count2++;
+//                 line += char1;
 //             }
+//             count2++;
 //         }
 //         console.log(line);    
 //         count1++;    
@@ -80,7 +78,7 @@
 
 // box(5, 8, '*', ' ');
 
-// Exercise 4:
+// // Exercise 4:
 // var banner = function (message, char) {
 //     var count1 = 0;
 //     while (count1 < 3) {
@@ -88,7 +86,7 @@
 //         if (count1 === 0 || count1 === 2) {
 //             var count2 = 0;
 //             while (count2 < (message.length + 4)) {
-//                 line = line + char;
+//                 line += char;
 //                 count2++;
 //             }
 //         }
@@ -119,55 +117,118 @@
 
 
 // Exercise 6:
-var getOffset = function (offset, abc, letter) {
-    var count = 0;
-    while (true) {
-        if (letter === abc[count]) {
-            if (offset < 0) {
-                if (offset < (-1 * count)) {
-                    var newOffset = (abc.length + (count + offset));
-                    return abc[newOffset];
-                }
-                else {
-                    return abc[count + offset];
-                }
-            }
-            var position = (abc.length - count);
-            if (offset > position) {
-                var newOffset = (offset - position);
-                return abc[newOffset];
-            }
-            else {
-                return abc[count + offset];
-            }        
-        }
-        count++;    
-    }
-};
 
-var cipher = function (string, offset) {
-    var lowerAbc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    var upperAbc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+// // Takes three arguments from cipher() and determines the replacement letter, based on the offset
+// var getOffset = function (offset, abc, letter) {
+//     var count = 0;
+//     while (true) {
+//         if (letter === abc[count]) {
+//             var position = (abc.length - count);
+
+//             // Checks to see if the number is being decrypted
+//             if (offset < 0) {
+//                 if (offset < (-1 * count)) {
+//                     var newOffset = (abc.length + (count + offset));
+//                     return abc[newOffset];
+//                 }
+//             }
+//             // Checks to see if number is being encrypted
+//             if (offset > position) {
+//                 var newOffset = (offset - position);
+//                 return abc[newOffset];
+//             }
+//             else {
+//                 return abc[count + offset];
+//             }        
+//         }
+//         count++;    
+//     }
+// };
+// // Finds the case of the letter and uses getOffset() to determine the replacement letter
+// // Returns the full encrypted message
+// var cipher = function (string, offset) {
+//     var lowerAbc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//     var upperAbc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//     var newString = '';
+
+//     for (var letter of string) {
+//         if (lowerAbc.includes(letter)) {
+//             newString += getOffset(offset, lowerAbc, letter);
+//         }
+//         else if (upperAbc.includes(letter)) {
+//             newString += getOffset(offset, upperAbc, letter);
+//         }
+//         else {
+//             newString += letter;
+//         }
+//     }
+//     return newString;
+// };
+
+// // Takes a message and encrypts it according to the offset
+// var encrypted = cipher('I am the master of my fate, I am the captain of my soul.', 10);
+
+// console.log(encrypted);
+
+// // Takes an encrypted message and returns the original
+// var solved = cipher(encrypted, -10);
+
+// console.log(solved);
+
+
+// // Exercise 7:
+// var leet = function (string) {
+//     var leetAbc = ['a', 'e', 'g', 'i', 'o', 's', 't'];
+//     var leetNum = ['4', '3', '6', '1', '0', '5', '7'];
+//     var newString = '';
+
+//     for (var letter of string) {
+//         letter = letter.toLowerCase();
+//         if (leetAbc.includes(letter)) {
+//             var count = 0;
+//             while (count < leetNum.length) {
+//                 if (letter === leetAbc[count]) {
+//                     newString += leetNum[count];
+//                     break;
+//                 }
+//                 else {
+//                     count++;
+//                 }
+//             }
+//         }
+//         else {
+//             newString = newString + letter;
+//         }
+//     }
+//     return newString;
+// };
+
+// var message = leet('I am the master of my fate; I am the captain of my soul.');
+// console.log(message);
+
+// var isEven = function(num) {
+//     return num % 2 === 0;
+// };
+
+// Exercise 8
+
+var extendLongVowels = function (string, vowels, length) {
     var newString = '';
-
+    var previousLetter = '';
+    var vowelsToLengthen = vowels;
     for (var letter of string) {
-        if (lowerAbc.includes(letter)) {
-            newString = newString + getOffset(offset, lowerAbc, letter);
+        if (vowelsToLengthen.includes(letter) && letter === previousLetter) {
+            for (var i = 1; i < length; i++) {
+                newString += letter
+            }
         }
-        else if (upperAbc.includes(letter)) {
-            newString = newString + getOffset(offset, upperAbc, letter);
-        }
-        else {
-            newString = newString + letter;
+        else { 
+            newString += letter;
+            previousLetter = letter.toLowerCase();
         }
     }
     return newString;
 };
 
-var encrypted = cipher('Genius without education is like silver in the mine', 10);
-
-console.log(encrypted);
-
-var solved = cipher('Qoxsec gsdryed onemkdsyx sc vsuo csvfob sx dro wsxo', -10);
-
-console.log(solved);
+var test = extendLongVowels('Ooh, have some cheese, Gromit!', ['e', 'o'], 5);
+console.log(test);
